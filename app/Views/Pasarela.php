55a7059da26598c364f7271ab1a37d5175cee419
jesestsana93 <jesestsana93@hotmail.com>
2022-08-02 14:46:17 -0500

    Integracion de archivos de mercado pago con composer

--- app/Views/Pasarela.php
+++ app/Views/Pasarela.php
@@ -10,9 +10,6 @@
 
 </head>
 <body>
-    <!-- SDK de paypal. Remplazar "test" con el client ID de la aplicacion sandbox de prueba -->
-    <!-- <script src="https://www.paypal.com/sdk/js?client-id=test&currency=MXN"></script> -->
-    <script src="https://www.paypal.com/sdk/js?client-id=AbwiWVYj1M9Du_1g30QLLF8qY6hWkNMdRS7M3fcrZPqCvzdkUgMjyla_swf5kvzKWwVnkE91mH7meRxC&currency=MXN"></script>
 
     <div class="container">
         <div class="row">
@@ -20,14 +17,18 @@
                 <h1 class="text-center">Pasarelas de pago</h1>
                 <!-- Set up a container element for the button -->
                 <div id="paypal-button-container"></div>
+                <div class="cho-container"></div>
             </div>
         </div>
     </div>
-    
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
+    <!-- ******************* SDK de paypal **************** -->
+    <!-- Remplazar "test" con el client ID de la aplicacion sandbox de prueba -->
+    <!-- <script src="https://www.paypal.com/sdk/js?client-id=test&currency=MXN"></script> -->
+    <script src="https://www.paypal.com/sdk/js?client-id=AbwiWVYj1M9Du_1g30QLLF8qY6hWkNMdRS7M3fcrZPqCvzdkUgMjyla_swf5kvzKWwVnkE91mH7meRxC&currency=MXN"></script>
 
     <script>
         paypal.Buttons({
@@ -73,7 +74,7 @@
                 console.log(data); //orderID
             },
         }).render('#paypal-button-container');
-
     </script>
+
 </body>
 </html>
